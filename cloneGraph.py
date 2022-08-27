9b2ba4ba8a89a52f5ea63bdb8a7b95f69f4ccc38
Kseniia Korotenko <xeniacinnamon@gmail.com>
2022-08-26 21:22:32 -0500

    clone graph: first-glance/naive solution

--- cloneGraph.py
+++ cloneGraph.py
@@ -6,9 +6,9 @@
 from collections import deque
 
 class Node:
-    def __init__(self, val = 0, neighbors = None)
-    self.val = val
-    self.neighbors = neighbors if neighbors is not None else []
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
 
 # Working solution: yet to be made cleaner and more succinct!
 
