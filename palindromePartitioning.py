7a10fb49ae303944eb6f93ac8b10273d14ed33b3
Kseniia Korotenko <xeniacinnamon@gmail.com>
2022-09-04 22:36:20 -0500

    palindrome partitioning: explained an alternative approach

--- palindromePartitioning.py
+++ palindromePartitioning.py
@@ -59,11 +59,23 @@ class Solution:
 
         # Time complexity: O(n * 2^n), where n is the length of the string s. This
         # is the worst-case time complexity where all the possible substrings
-        # are palindromes (id est, the string contains just one letter repeated many
-        # times). If we look at the recursive tree for this case, it was 2^n total
+        # are palindromes (id est, the string contains just one letter repeated n
+        # times). If we look at the recursive tree for this case, it has 2^n total
         # nodes (2^n possible substrings). For each substring, it takes O(n) time to
-        # generate substring and determing if it is a palindrome or not. This gives us
-        # time complexity O(n*2^n).
+        # generate substring and O(n) time to determine if it is a palindrome or not. 
+        # This gives us time complexity O(n*2^n).
+
+        # Idea: notice how in this approach, we are repeatedly iterating over the
+        # same substring multiple times, and the result is always the same. These
+        # are overlapping problems -> we could further optimize this approach by using
+        # dynamic programming to determine if a string is a palindrome in constant time.
+
+        # So we can turn this approach into a dynamic programming one by creating
+        # a 2D DP array. This would bump space complexity to O(n^2). However,
+        # given than substring generation costs O(n) for an n-length substring,
+        # time complexity stays O(n*2^n): we just throw out one n-factor that stood
+        # for checking whether the string is a palindrome using two pointers. So we multiply
+        # the total expression by n one time fewer, but asymptotically the time stays the same.
         
 
 if __name__ == "__main__":
